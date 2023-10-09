15cafe55f6462e122a908446cae25244fc4fac42
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-10-10 00:05:00 +0530

    revision

--- sync_async.js
+++ sync_async.js
@@ -1,5 +1,23 @@
 console.log("hey"); //sync
-setTimeout(function () { //async
+setTimeout(function () {
+  //async
   console.log("hey2");
 }, 2000);
 console.log("hey3"); //sync
+
+//promises 
+var ans = new Promise((res, rej) => {
+  if (true) {
+    return res();
+  } else {
+    return rej();
+  }
+});
+
+ans
+  .then(function () {
+    console.log("resolved");
+  })
+  .catch(function () {
+    console.log("reject");
+  });
