92c2183a55d51dcc7177f216c10e8096cc0f5811
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-10-10 00:26:34 +0530

    prac

--- sync_async.js
+++ sync_async.js
@@ -5,9 +5,9 @@ setTimeout(function () {
 }, 2000);
 console.log("hey3"); //sync
 
-//promises 
+//promises
 var ans = new Promise((res, rej) => {
-  if (true) {
+  if (false) {
     return res();
   } else {
     return rej();
@@ -20,4 +20,21 @@ ans
   })
   .catch(function () {
     console.log("reject");
+  });
+
+//user will ask for a number between 0 and 9 if the number is below 5 then resolve else reject
+var ans = new Promise((res, rej) => {
+    var a = Math.floor(Math.random()*10);
+  if (a >= 0 && a <= 5) {
+    return res();
+  } else {
+    return rej(a);  
+  }
+});
+ans
+  .then(function () {
+    console.log("OKAY! number is good ");
+  })
+  .catch(function () {
+    console.log("number is greater than 5");
   });
